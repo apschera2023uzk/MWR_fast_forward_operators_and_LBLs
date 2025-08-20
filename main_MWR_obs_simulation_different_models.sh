36fe9893fc3762fdd3fb93e0ba001fcd6a62402b
Alexander Pschera <alexander.pschera@posteo.de>
2025-08-20 17:09:05 +0200

    Added ARMS-gb wrapper - ran ARMS-gb on all zenith files.

--- main_MWR_obs_simulation_different_models.sh
+++ main_MWR_obs_simulation_different_models.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
+# RTTOV-gb processing:
 ./python_src/rttov-gb_wrapper/preprocessing4rttov-gb_4zen.py &&
 ./python_src/rttov-gb_wrapper/run_rttov-gb_on_rs4zen.py &&
+
+# Prtlib processing:
 ./python_src/run_pyrtlib/run_pyrtlib_on_all.py &&
-# Add ARMS-gb processing
+
+# ARMS-gb processing:
+./python_src/arms-gb-wrapper/preprocessing4armsgb.py &&
+cd /home/aki/armsgb/Obs_Sim_armsgb &&
+export FC=ifx &&
+make clean &&
+make &&
+./FWD_Test &&
+
+# Summarize and plot:
 ./python_src/merge_data_into_netCDF/merge2nc.py &&
 ./python_src/plot_scripts/plot_TB-scatter_MWR.py
 
